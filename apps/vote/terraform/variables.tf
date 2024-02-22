94c4fe43d366abf55a5b04695b8480e575141283
Robin Smorenburg <robin@robino.io>
2024-02-22 13:18:52 +0100

    Remove unused variable

--- apps/vote/terraform/variables.tf
+++ apps/vote/terraform/variables.tf
@@ -13,28 +13,13 @@ variable "location" {
 variable "location_abbreviation" {
   description = "Optional. The abbreviation of the location."
   type        = map(string)
-  default     = {
+  default = {
     "westeurope"  = "weu"
     "northeurope" = "neu"
     "eastus"      = "eus"
     "westus"      = "wus"
     "ukwest"      = "ukw"
     "uksouth"     = "uks"
-  }
-}
-
-variable "environment" {
-  description = "Required. The environment for the resources."
-  type        = string
-}
-
-variable "environment_abbreviation" {
-  description = "Optional. The abbreviation of the environment."
-  type        = map(string)
-  default     = {
-    "development" = "dev"
-    "staging"     = "stage"
-    "production"  = "prod"
   }
 }
 
